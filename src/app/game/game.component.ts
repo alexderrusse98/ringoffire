26f3a3dbf53a454b3a28760f65d2bbb20381e932
alexderrusse98 <alex.weiswasser.com@gmail.com>
2025-12-10 11:12:00 +0100

    add btn

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,16 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Game } from '../models/game';
 import { PlayerComponent } from '../player/player.component';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule,
-    PlayerComponent
+  imports: [
+    CommonModule,
+    PlayerComponent,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
+
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
   currentCards: string = '';
