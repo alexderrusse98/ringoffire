fbe107712c074083ff7f1417ba628d24f8d67139
alexderrusse98 <alex.weiswasser.com@gmail.com>
2025-12-10 01:37:03 +0100

    new ts and shuffle modi

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,23 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Game } from '../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
-  pickCardAnimation = false
-  constructor() {
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCards: string = '';
+  game!: Game;
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+    console.log(this.game);
+    
+  }
+
+  newGame() {
+    this.game = new Game();
   }
 
   takeCard() {
+    this.currentCards = this.game.stack.pop() || '';
     this.pickCardAnimation = true;
-
   }
 }
--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -1,6 +1,24 @@
 export class Game {
-    public players: string[] = [];
-    public stack: string[] = [];
-    public playedCards: string[] = [];
-    public currentPlayer: number = 0;
+  public players: string[] = [];
+  public stack: string[] = [];
+  public playedCards: string[] = [];
+  public currentPlayer: number = 0;
+
+  constructor() {
+    for (let i = 0; i < 14; i++) {
+      this.stack.push('spade_' + i);
+      this.stack.push('heart_' + i);
+      this.stack.push('clubs_' + i);
+      this.stack.push('diamonds_' + i);
+    }
+
+    this.shuffle(this.stack);  
+  }
+
+  shuffle(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
 }
