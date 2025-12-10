aa665af73d5a062bdd0476fdf972071ce426678b
alexderrusse98 <alex.weiswasser.com@gmail.com>
2025-12-10 10:25:47 +0100

    new c player

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Game } from '../models/game';
+import { PlayerComponent } from '../player/player.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+    PlayerComponent
+  ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
